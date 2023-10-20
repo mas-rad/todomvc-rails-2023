4e938467478d3db5c8291e944ce2256303e5fd50
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2023-10-20 16:16:47 +0200

    Switch Application System test to use headless_firefox driver
    This patch switches using `firefox-esr` packager for Application System
    tests, which is better working in the docked environment.
    
    This custom docker image will be used:
    ghcr.io/mas-rad/rails-cli-firefox-esr:latest

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,8 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :headless_firefox, screen_size: [1400, 1400]
 end
+
+# Remove "Capybara starting Puma..." test logs
+Capybara.server = :puma, { Silent: true }
