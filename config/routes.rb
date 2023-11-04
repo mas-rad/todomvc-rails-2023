0ba807f7aacb4d037cc82b15134cfe0b2e281eb0
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2023-11-04 11:40:52 +0100

    Move new form to main page
    This patch moves the new form to the main page in the new `header`
    section as it must be present at all time.
    
    Some other changes:
    - Application CSS has been updated by the standard [todomvc-app-css](https://github.com/tastejs/todomvc-app-css/blob/master/index.css) one
    - The page `<body>` content has been wrapped in a `<section class="todoapp">` as specified
    - A new `layouts/_header.html.erb` has been introduced to segment the app part better
    - The `<h1>` HTML tags have been removed from all todos views as it's now part of the header
    - The `todos/new.html.erb` view has been dropped as the new form is now in the header
    - The `/todos/new` route has been removed as well
    - A remote new form with just the `title` input has been added in the header
    - The `title` input uses the `autofocus` attribute
    
    App specification
    -----------------
    
    - https://github.com/tastejs/todomvc/blob/master/app-spec.md#new-todo
    
    Documentation
    -------------
    
    - https://guides.rubyonrails.org/form_helpers.html
    - https://api.rubyonrails.org/classes/ActionView/Helpers/FormHelper.html#method-i-form_with
    - https://api.rubyonrails.org/classes/ActionView/Helpers/FormBuilder.html#method-i-text_field

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root to: redirect("/todos")
 
-  resources :todos
+  resources :todos, except: [:new]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
