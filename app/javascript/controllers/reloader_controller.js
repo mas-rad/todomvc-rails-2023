cb6e2748cc6346a8e370f5407567387d7938422e
Rafael Shroom <68326435+shroom-r@users.noreply.github.com>
2023-12-14 20:08:50 +0100

    exercise-14

--- app/javascript/controllers/reloader_controller.js
+++ app/javascript/controllers/reloader_controller.js
@@ -2,9 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   reload(event) {
-    // event.preventDefault()
-    event.stopImmediatePropagation()
-    console.log("reloader")
     Turbo.visit(window.location.href)
   }
 }
--- app/javascript/controllers/submitable_controller.js
+++ app/javascript/controllers/submitable_controller.js
@@ -2,7 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   submit(event) {
-    console.log("blur")
     this.element.requestSubmit()
   }
 }
